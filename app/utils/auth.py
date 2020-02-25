b359160e18f900618309f048e1377ca0b704554f
Kosuke Uchida <k_uchida@ardito.co.jp>
2020-02-25 10:29:16 +0900

    テスト

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -2,14 +2,17 @@ import functools
 from flask import request
 
 
+def auth_impl(api, func, *args, **kwargs):
+    if request.headers.get('x-company-id') is None:
+        api.abort(400, 'X-Company-ID が未指定')
+    else:
+        return func(*args, **kwargs)
+
 def auth(api):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            if request.headers.get('x-company-id') is None:
-                api.abort(400, 'X-Company-ID が未指定')
-            else:
-                return func(*args, **kwargs)
+            return auth_impl(api, func, *args, **kwargs)
 
         return wrapper
 
